2980f0a40ebfc2fe28b82597de07216f07962913
em1rR <46686523+em1rR@users.noreply.github.com>
2019-01-22 12:53:47 +0300

    Add files via upload

---
+++ deneme 2.py
@@ -0,0 +1,51 @@
+x = input("Lütfen cümle girniiz...")
+
+q = "a"
+w = "e"
+e = "i"
+r = "ı"
+t = "o"
+y = "ö"
+z = "u"
+d = "ü"
+
+sayı1 = x.count(q)
+if(sayı1 > 0):
+    sayı1 += 1
+sayı2 = x.count(w)
+if(sayı2 > 0):
+    sayı2 += 1
+sayı3 = x.count(e)
+if(sayı3 > 0):
+    sayı3 += 1
+sayı4 = x.count(r)
+if(sayı4 > 0):
+    sayı4 += 1
+sayı5 = x.count(t)
+if(sayı5 > 0):
+    sayı5 += 1
+sayı6 = x.count(y)
+if(sayı6 > 0):
+    sayı6 += 1
+sayı7 = x.count(z)
+if(sayı7 > 0):
+    sayı7 += 1
+sayı8 = x.count(d)
+if(sayı8 > 0):
+    sayı8 += 1
+
+sonuc = sayı1+sayı2+sayı3+sayı4+sayı5+sayı6+sayı7+sayı8
+
+print(sayı1,"tane a",sayı2,"tane e",sayı3,"tane i",sayı4,"tane ı",sayı5,"tane o",sayı6,"tane ö",sayı7,"tane u",sayı8,"tane ü","harfi vardı.")
+
+
+
+
+
+
+
+        
+        
+        
+
+
